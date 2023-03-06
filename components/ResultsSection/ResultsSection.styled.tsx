717d6f7aa6a0c0c6bcbac46077119a532c0a693d
Niels Louwes <niels.louwes@and.digital>
2023-03-06 14:08:26 +0100

    Added new font through nextjs

--- components/ResultsSection/ResultsSection.styled.tsx
+++ components/ResultsSection/ResultsSection.styled.tsx
@@ -1,13 +1,15 @@
 import styled from "styled-components";
 
 const ScoreContainer = styled.div`
-  border: 1px solid red;
   width: 80%;
   margin-left: 10%;
   margin-right: 10%;
 `;
 
-export const Text = styled.p``;
+export const Text = styled.p`
+text-align: right;
+padding-right: 55px;
+`;
 
 export const ResultsSectionStyled = {
   ScoreContainer,
